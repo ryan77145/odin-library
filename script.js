70a23d0f1487bd3def2dc6745a61dbcfa3f92f43
Ryan Shill <ryshill1996@gmail.com>
2026-08-18 08:10:51 -0500

    added a gitignore

---
+++ script.js
@@ -0,0 +1,12 @@
+class Books {
+    constructor() {
+        this.name = "Books"
+    }
+}
+
+function createBook(bookName, bookAuthor) {
+    let uuid = book.crypto.randomUUID();
+
+}
+
+console.log(uuid);
