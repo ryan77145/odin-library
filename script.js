423e20045574549d24a56e65f62926c729f544a4
ry77145 <ryan@ryanshill.com>
2026-08-19 17:26:01 -0500

    added the functionality to change book color status to read and unread

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 // Global Declarations
 const library = [];
-const shelf = document.querySelector('.shelf');
+const shelf = document.querySelector(".bookShelf");
+const button = document.querySelector(".btn");
+const addBook = document.querySelector(".addBook");
+const title = document.querySelector("#bookTitle");
+const author = document.querySelector("#bookAuthor");
+const pages = document.querySelector("#numberOfPages");
+const readYes = document.querySelector("#readYes");
+const readNo = document.querySelector("#readNo");
+const submit = document.querySelector(".submit");
 
 class Books {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function createBook(bookTitle, bookAuthor) {
-    const book = new Books(bookTitle, bookAuthor);
-    book.id = crypto.randomUUID();
-    book.title = bookTitle;
-    book.author = bookAuthor;
-    
-    library.push(book);
-    return library
+button.addEventListener("click", () => {
+  addBook.classList.toggle("active");
+});
+
+addBook.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBook.classList.toggle("active");
+  createBook(title.value, author.value, pages.value, readYes.checked);
+  displayBooks();
+  addBook.reset();
+});
+
+function createBook(bookTitle, bookAuthor, numOfPages, read) {
+  const book = new Books(bookTitle, bookAuthor, numOfPages, read);
+  book.id = crypto.randomUUID();
+
+  library.push(book);
+  return library;
 }
 
-const displayBooks = function() {
-    shelf.innerHTML = '';
-    library.forEach(function(book) {
-        const individualBooks = document.createElement('div');
-        individualBooks.setAttribute("class", "books");
-        individualBooks.textContent = `${book.title}, ${book.author}`;
+const displayBooks = function () {
+  shelf.innerHTML = "";
+  library.forEach(function (book) {
+    const individualBooks = document.createElement("div");
+    const bookText = document.createElement("p");
+    individualBooks.setAttribute("class", "book");
+    bookText.setAttribute("class", "bookTitle");
+    bookText.textContent = `${book.title}, ${book.author}`;
+    individualBooks.appendChild(bookText);
+
+    // Place listener INSIDE the loop, targeting the DOM element
+    individualBooks.addEventListener("click", () => {
+      individualBooks.classList.toggle("active");
+    });
+
     shelf.appendChild(individualBooks);
-})}
+  });
+};
 
-createBook('Harry Potter', 'JK Rowling');
-createBook('Green Eggs and Ham', 'Dr. Suess');
 displayBooks();
-
-
-
